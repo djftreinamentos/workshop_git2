96c7fa62daa44c8a708acbf91705965f39b1cfc4
Felipe da Rosa Oliveira <felipe.darosaoliveira@gmail.com>
2021-06-11 20:49:29 -0300

    main

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+/**
+ * Módulo App
+ * 
+ * Neste módulo estão as principais funcionalidades da aplicação.
+ * 
+ */
+
+
+
+/**
+ * @description Esta função tem por objetivo apresentar uma mensagem de boas vindas. 
+ * @author João da Silva 
+ * */
 function hello(){
     alert('Hello');
 }
